ae720c42fce70edbcad62257b9f3ce097b663968
GabrielOlufemi <kusorogabriel@gmail.com>
2026-03-26 08:46:57 +0100

    missing comma addition

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ app.add_middleware(
         "http://localhost:5500",  # VS Code Live Server
         "http://127.0.0.1:5500",
         "http://localhost:8080",
-        "https://eave-frontend.vercel.app"
+        "https://eave-frontend.vercel.app",
         "https://eave-frontend-miyhgxrnn-gabriels-projects-3cafcbd3.vercel.app",
         "https://eave-health-frontend.vercel.app",
     ],
